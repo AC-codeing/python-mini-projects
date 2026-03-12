5c6b6b269003907abab1ad8881eb9c8006df90b2
AC-codeing <angelinecandra61933@gmail.com>
2026-03-13 00:01:23 +0700

    To-Do-List / to-do-list.py
    CLI Program to add, view, and delete tasks.

--- to-do-list.py
+++ to-do-list.py
@@ -1,27 +1,27 @@
-print("\n=== To-do list ===")
-print("\n  1.Tambah Tugas")
-print("  2.Lihat Tugas")
-print("  3.Hapus Tugas")
-print("  4.Keluar")
-
 tugas_list = []
 while True:
+    print("\n=== To-do list ===")
+    print("\n  1.Tambah Tugas")
+    print("  2.Lihat Tugas")
+    print("  3.Hapus Tugas")
+    print("  4.Keluar")
+
     a = input("\nSilahkan pilih menu di atas (1-4): ")
 
     if a == '1':
         tugas = input("Masukkan tugas baru : ")
         tugas_list.append(tugas)
-        print("Tugas berhasil di tambah")
+        print("\nTugas berhasil di tambah")
     elif a == '2':
         if len(tugas_list) == 0:
-            print('Belum ada tugas')
+            print('\nBelum ada tugas')
         else:    
             print("Daftar tugas : ")
             for i, t in enumerate(tugas_list, start=1):
                 print(f'{i}. {t}')
     elif a == '3':
         if len(tugas_list) == 0:
-            print("Tidak ada tugas yang bisa dihapus")
+            print("\nTidak ada tugas yang bisa dihapus")
         else:
             print('Daftar tugas : ')
             for i, t in enumerate(tugas_list, start=1):
@@ -29,11 +29,11 @@ while True:
             hapus = int(input("Tugas nomor berapa yang mau dihapus: "))
             if 1 <= hapus <= len(tugas_list):
                 tugas_list.pop(hapus - 1)
-                print('Tugas berhasil dihapus')
+                print('\nTugas berhasil dihapus')
             else:
-                print("Nomor tugas tidak valid")
+                print("\nNomor tugas tidak valid")
     elif a == '4':                        
-        print('Berhasil keluar')
+        print('\nBerhasil keluar')
         break
     else:
         print('Menu tidak ada')    
