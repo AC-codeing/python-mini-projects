ea2a082de8b67e888f5dc7c8f93c7ee689f7bf0a
AC-codeing <angelinecandra61933@gmail.com>
2026-03-12 23:29:06 +0700

    calculator / calculator.py
    Simple calculator with add, subtract, multiply, divide.

--- calculator.py
+++ calculator.py
@@ -5,22 +5,24 @@ def bagi(x, y): return x / y if y != 0 else "error"
 
 while True:
     try:
-        print("=== Kalkulator Sederhana ===")
-        print('1.Tambah 2.Kurang 3.Kali 4.Bagi')
-        a = input("Silahkan pilih menu di atas (1-4): ")
+        print("\n === Kalkulator Sederhana ===")
+        print('\n 1.Tambah 2.Kurang 3.Kali 4.Bagi 5.Keluar')
+        a = input("\nSilahkan pilih menu di atas (1-5): ")
 
         if a in ('1', '2', '3', '4'):
             n1 = float(input("Masukkan angka pertama: "))
             n2 = float(input("Masukkan angka kedua: "))
 
-        if a == '1':
-            print('Hasil : ', tambah(n1, n2))
-        elif a == '2':
-            print('Hasil : ', kurang(n1, n2))
-        elif a == '3':
-            print('Hasil : ', kali(n1, n2))
-        elif a == '4':
-            print('Hasil : ', bagi(n1, n2))
+            if a == '1':
+                print(f'{n1} + {n2} = ', tambah(n1, n2))
+            elif a == '2':
+                print(f'{n1} - {n2} = ', kurang(n1, n2))
+            elif a == '3':
+                print(f'{n1} x {n2} = ', kali(n1, n2))
+            elif a == '4':
+                print(f'{n1} / {n2} = ', bagi(n1, n2))
+        else:
+            break    
     except ValueError: 
         print("Input salah! Masukkan angka yang valid")
 else:
